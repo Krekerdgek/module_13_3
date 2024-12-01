9ed53d0a8e99ae5f5a75a8c3b18f29f08bb2aad6
Krekerdgek <dimos-79@mail.ru>
2024-12-01 18:58:10 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,19 @@
+# Задача "Он мне ответил!"
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = '8089404457:AAFRCwb80AfVsJbrh1QI-pp9Rwf2o_9Yqq8'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
